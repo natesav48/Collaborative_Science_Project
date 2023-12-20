fc580d04a97a5edb47a6a047a04f01eb7f806c2b
natesavarese <nathansavarese@stu.postoakschool.org>
2023-12-20 12:38:54 -0600

    fixed minor error

--- Main.py
+++ Main.py
@@ -5,8 +5,8 @@ end_weights = []
 def get_data():
     while True:
         data_input_name = input("Enter the name of the fruit: ")
-        data_input_start = float(input(f"Enter the starting weight of",data_input_name))
-        data_input_end = float(input(f"Enter the ending weight of", data_input_name))
+        data_input_start = float(input("Enter the starting weight of"+data_input_name))
+        data_input_end = float(input("Enter the ending weight of "+ data_input_name))
 
         fruits.append(data_input_name)
         start_weights.append(data_input_start)
